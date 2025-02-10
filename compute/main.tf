7583ea23b565711ee37a3eab3d381020b472ee29
suhalthakkar <suhalthakkar189@gmail.com>
2025-02-10 13:40:18 +0000

    Add Terraform GitHub Actions deployment workflow - 3

--- compute/main.tf
+++ compute/main.tf
@@ -10,21 +10,23 @@ terraform {
   }
 }
 
-resource "aws_security_group" "app_sg" {
-  vpc_id = var.vpc_id
+resource "aws_security_group" "asg_sg" {
+  vpc_id = aws_vpc.main.id
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
+} 
 
 resource "aws_lb" "alb" {
   name               = "app-alb"
@@ -70,15 +72,13 @@ resource "aws_launch_template" "app_lt" {
 }
 
 resource "aws_autoscaling_group" "asg" {
+  launch_configuration = aws_launch_configuration.main.id
+  vpc_zone_identifier  = var.public_subnet_ids
+  min_size             = 1
+  max_size             = 3
   desired_capacity     = 2
-  max_size            = 4
-  min_size            = 2
-  vpc_zone_identifier = var.public_subnet_ids
+  target_group_arns    = [module.alb.alb_target_group_arn]
 
-  launch_template {
-    id      = aws_launch_template.app_lt.id
-    version = "$Latest"
-  }
-
-  target_group_arns = [aws_lb_target_group.tg.arn]
+  // Correct security group reference
+  vpc_security_group_ids = [aws_security_group.asg_sg.id]
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -29,4 +29,18 @@ resource "aws_subnet" "private" {
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
 data "aws_availability_zones" "available" {}
